014b05ee3ff7247623fe2b680680a8d862501e33
okorkov <alex.okorkov@gmail.com>
2020-12-10 20:10:18 -0800

    commit tree c2998a96fe242351aef50a3100e5e8696e4056a3

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    fullname = "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    char = Character.all.where("actor_id = #{self.id}")[0]
+    show = Show.all.where("id = #{char.show_id}")[0]
+    "#{char.name} - #{show.name}"
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    characters = Character.all.where("show_id = #{self.id}")[0]
+    actors = Actor.all.where("id = #{characters.actor_id}")
+    actors.map {|actor| actor.full_name}
+  end
   
 end
